5f2c6c6833b47df4d868affcb295edea4d2ea696
Alex Errington <contact@alex-errington.co.uk>
2016-03-15 20:06:13 +0000

    mongo connection

--- server.js
+++ server.js
@@ -3,13 +3,28 @@
 require('dotenv').load();
 var express = require('express');
 var routes = require(process.cwd() + '/app/routes/index.js');
+var mongo = require('mongodb').MongoClient;
 
 var app = express();
 app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
 
-routes(app);
-
 var port = process.env.PORT || 8080;
-app.listen(port,  function () {
-	console.log('Node.js listening on port ' + port + '...');
+var mongoUrl = process.env.MONGO_URL || 'mongodb://localhost:27017/urlshorten';
+
+mongo.connect(mongoUrl, function (err, db) {
+
+    if (err) {
+        throw new Error('Database failed to connect!');
+    } else {
+        console.log('MongoDB successfully connected on port 27017.');
+    }
+
+    routes(app, db);
+
+    
+    app.listen(port,  function () {
+    	console.log('Node.js listening on port ' + port + '...');
+    });
+
 });
+
