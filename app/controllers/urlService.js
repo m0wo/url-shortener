3bed911a8aa423cce956ea8e51353a255b0527a2
Alex Errington <contact@alex-errington.co.uk>
2016-03-16 19:11:13 +0000

    storing urls, generating shorturl key

--- app/controllers/urlService.js
+++ app/controllers/urlService.js
@@ -3,17 +3,39 @@
 function urlService (db) {
     var urls = db.collection('urlsTesting');    //remove Testing on prod.
     
-    this.shorten = function(url){
-        console.log(url);
-        
-        urls.insertOne({"original_url": url}, function(err, result){
-           if(err == null){
-            console.log("added new url");
-            //todo: callback so the api can display the result
-           return;
-           }
-        });
+    function generateKey(){
+        return Math.random().toString(36).substring(7);
     }
+    
+    function validateUrl(url){
+        return url.match('(https?:\/\/(?:www\.|(?!www))[^\s\.]+\.[^\s]{2,}|www\.[^\s]+\.[^\s]{2,})');
+    }
+    
+
+    this.shorten = function(url, callback){
+        if(validateUrl(url) != null){
+            urls.insertOne({"original_url": url, "short_url": generateKey()}, function(err, result){
+               if(err == null){
+                var obj = {"original_url": result.ops[0].original_url, "short_url": result.ops[0].short_url};
+                callback(obj);
+               }
+            });
+        }
+    }
+    
+    this.retrieve = function(shortUrl){
+           var cursor =urls.find( { "short_url": shortUrl } );
+           cursor.each(function(err, doc) {
+              if(err != null){
+                  console.log(err);
+              }
+              if (doc != null) {
+                 console.log(doc);
+                 return true;
+              }
+           });
+    }
+    
     
     this.getAll = function(){
         var cursor = urls.find( );
--- app/routes/index.js
+++ app/routes/index.js
@@ -10,14 +10,23 @@ module.exports = function (app, db) {
             res.send("thank mr skeltal");
         });
         
-    app.route('/new/:url')
+    app.route('/new/*')
         .get(function(req, res){
-           var url = req.params.url;
-           urlService.shorten(url);
+           var url = req.params[0];
+           //res.send(urlService.shorten(url));
+           urlService.shorten(url, function(result){
+               res.send(result);
+           })
+    });
+        
+    app.route('/:key')
+        .get(function(req, res){
+           var key = req.params.key;
+           res.send(urlService.retrieve(key));
         });
         
-    app.route('/all')
+    app.route('/api/all')
         .get(function(req, res){
-            urlService.getAll();
+            res.send(urlService.getAll());
         });
 };
