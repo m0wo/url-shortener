10aa4e4266b0e14e66fcc82e323b2df455d567e3
Alex Errington <contact@alex-errington.co.uk>
2016-03-16 08:04:15 +0000

    Talking to db

--- app/controllers/urlService.js
+++ app/controllers/urlService.js
@@ -1,6 +1,31 @@
 'use strict';
 
 function urlService (db) {
+    var urls = db.collection('urlsTesting');    //remove Testing on prod.
+    
+    this.shorten = function(url){
+        console.log(url);
+        
+        urls.insertOne({"original_url": url}, function(err, result){
+           if(err == null){
+            console.log("added new url");
+            //todo: callback so the api can display the result
+           return;
+           }
+        });
+    }
+    
+    this.getAll = function(){
+        var cursor = urls.find( );
+        cursor.each(function(err, doc){
+            if(err != null){
+                console.log(err);
+            }
+            if(doc != null){
+                console.log(doc);
+            }
+        })
+    }
     
 }
 
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,8 +1,23 @@
 'use strict';
 
+var UrlService = require(process.cwd() + '/app/controllers/urlService.js');
+
 module.exports = function (app, db) {
+    var urlService = new UrlService(db);
+    
     app.route('/')
         .get(function (req, res) {
             res.send("thank mr skeltal");
+        });
+        
+    app.route('/new/:url')
+        .get(function(req, res){
+           var url = req.params.url;
+           urlService.shorten(url);
+        });
+        
+    app.route('/all')
+        .get(function(req, res){
+            urlService.getAll();
         });
 };
